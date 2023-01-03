16f9450cfa8ea776335d2c7e831f7fe67878ab93
AleProsDev <alejandro.f.pros@gmail.com>
2023-01-03 19:01:38 -0300

    ejercicio2

---
+++ mi_primer_proyecto_Node/ejercicio2.js
@@ -0,0 +1,14 @@
+//Ejercicio 
+
+const lista = [
+    nombre = "Alejandro",
+    edad = 34,
+    isDesarrollador = false,
+    fechaNac = new Date(),
+    libroFavorito = {
+        titulo: "las rosas no son del viento",
+        autor: "Pablo kadinski",
+        fechaPubl: 20-2-2022,
+        url : "http//www.google.com.ar",
+    }
+]
