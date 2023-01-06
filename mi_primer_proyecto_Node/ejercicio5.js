059536a327de869f629712b04e15528a3fc8a496
AleProsDev <alejandro.f.pros@gmail.com>
2023-01-06 15:29:15 -0300

    ejercicio5

---
+++ mi_primer_proyecto_Node/ejercicio5.js
@@ -0,0 +1,9 @@
+let alturaInt = 165
+let alturaFloat = 1.64
+let pesoFloat = 84.4
+let alturaRound = Math.round(alturaFloat)
+let pesoRound = Math.floor(pesoFloat)
+let maxNumberJs = Number.MAX_VALUE;
+let maxNumber_num = (2 ** 1023) * 1.999999999999999
+let result = maxNumberJs === maxNumber_num;
+console.log(result);
